da505789d6594822610c5ebacaab8fa91ef997a8
ocolunga <oscar@ancile.dev>
2026-01-28 22:27:22 -0600

    Refactor macos-config-tools formula: update class name and resource URLs

--- Formula/macos-config-tools.rb
+++ Formula/macos-config-tools.rb
@@ -1,4 +1,4 @@
-class Mct < Formula
+class MacosConfigTools < Formula
   include Language::Python::Virtualenv
 
   desc "macOS Configuration Tools"
@@ -10,43 +10,48 @@ class Mct < Formula
   depends_on "python@3.12"
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/d3/b0/ef5a28e15a36dcb558228ca49d9d9120dc1191205c1334a734c7769fc487/macos_config_tools-0.2.1.tar.gz"
-    sha256 "f5ed91680f04de80797d4b48eadef8ff231d7e919568b19f68f6e14b23f4175f"
+    url "https://files.pythonhosted.org/packages/3d/fa/656b739db8587d7b5dfa22e22ed02566950fbfbcdc20311993483657a5c0/click-8.3.1.tar.gz"
+    sha256 "12ff4785d337a1bb490bb7e9c2b1ee5da3112e94a8622f26a6c77f5d2fc6842a"
   end
 
   resource "markdown-it-py" do
-    url "https://files.pythonhosted.org/packages/d3/b0/ef5a28e15a36dcb558228ca49d9d9120dc1191205c1334a734c7769fc487/macos_config_tools-0.2.1.tar.gz"
-    sha256 "f5ed91680f04de80797d4b48eadef8ff231d7e919568b19f68f6e14b23f4175f"
+    url "https://files.pythonhosted.org/packages/5b/f5/4ec618ed16cc4f8fb3b701563655a69816155e79e24a17b651541804721d/markdown_it_py-4.0.0.tar.gz"
+    sha256 "cb0a2b4aa34f932c007117b194e945bd74e0ec24133ceb5bac59009cda1cb9f3"
   end
 
   resource "mdurl" do
-    url "https://files.pythonhosted.org/packages/d3/b0/ef5a28e15a36dcb558228ca49d9d9120dc1191205c1334a734c7769fc487/macos_config_tools-0.2.1.tar.gz"
-    sha256 "f5ed91680f04de80797d4b48eadef8ff231d7e919568b19f68f6e14b23f4175f"
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
   end
 
   resource "pygments" do
-    url "https://files.pythonhosted.org/packages/d3/b0/ef5a28e15a36dcb558228ca49d9d9120dc1191205c1334a734c7769fc487/macos_config_tools-0.2.1.tar.gz"
-    sha256 "f5ed91680f04de80797d4b48eadef8ff231d7e919568b19f68f6e14b23f4175f"
+    url "https://files.pythonhosted.org/packages/b0/77/a5b8c569bf593b0140bde72ea885a803b82086995367bf2037de0159d924/pygments-2.19.2.tar.gz"
+    sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
+  end
+
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
+    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/d3/b0/ef5a28e15a36dcb558228ca49d9d9120dc1191205c1334a734c7769fc487/macos_config_tools-0.2.1.tar.gz"
-    sha256 "f5ed91680f04de80797d4b48eadef8ff231d7e919568b19f68f6e14b23f4175f"
+    url "https://files.pythonhosted.org/packages/a1/84/4831f881aa6ff3c976f6d6809b58cdfa350593ffc0dc3c58f5f6586780fb/rich-14.3.1.tar.gz"
+    sha256 "b8c5f568a3a749f9290ec6bddedf835cec33696bfc1e48bcfecb276c7386e4b8"
   end
 
   resource "shellingham" do
-    url "https://files.pythonhosted.org/packages/d3/b0/ef5a28e15a36dcb558228ca49d9d9120dc1191205c1334a734c7769fc487/macos_config_tools-0.2.1.tar.gz"
-    sha256 "f5ed91680f04de80797d4b48eadef8ff231d7e919568b19f68f6e14b23f4175f"
+    url "https://files.pythonhosted.org/packages/58/15/8b3609fd3830ef7b27b655beb4b4e9c62313a4e8da8c676e142cc210d58e/shellingham-1.5.4.tar.gz"
+    sha256 "8dbca0739d487e5bd35ab3ca4b36e11c4078f3a234bfce294b0a0291363404de"
   end
 
   resource "typer" do
-    url "https://files.pythonhosted.org/packages/d3/b0/ef5a28e15a36dcb558228ca49d9d9120dc1191205c1334a734c7769fc487/macos_config_tools-0.2.1.tar.gz"
-    sha256 "f5ed91680f04de80797d4b48eadef8ff231d7e919568b19f68f6e14b23f4175f"
+    url "https://files.pythonhosted.org/packages/36/bf/8825b5929afd84d0dabd606c67cd57b8388cb3ec385f7ef19c5cc2202069/typer-0.21.1.tar.gz"
+    sha256 "ea835607cd752343b6b2b7ce676893e5a0324082268b48f27aa058bdb7d2145d"
   end
 
   resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/d3/b0/ef5a28e15a36dcb558228ca49d9d9120dc1191205c1334a734c7769fc487/macos_config_tools-0.2.1.tar.gz"
-    sha256 "f5ed91680f04de80797d4b48eadef8ff231d7e919568b19f68f6e14b23f4175f"
+    url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
+    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
   end
 
   def install
@@ -56,4 +61,4 @@ class Mct < Formula
   test do
     system bin/"mct", "--help"
   end
-end 
+end
